eaa8ad691d8b21ed603a56c1ea11485276bc635b
Sebastian Piperis <sebastianpiperis01@gmail.com>
2024-02-08 16:17:25 -0500

    fixed stuff

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from flask import Flask, render_template, request, redirect, url_for, session
-from flask_login import LoginManager, UserMixin, login_user, login_required, current_user, logout_user
+from flask import Flask, render_template, request, session, redirect,  url_for
 import pandas as pd # used to parse excel file
 import requests
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import os
 import re
+import json
 
 
 
-load_dotenv() #calls env
 app = Flask(__name__) # creates a flask app instance in order to create routes for the app
 app.secret_key = 'SITE_KEY' # gets the app's secret key from env
 
+load_dotenv()
 
 
 # excel variables 
@@ -25,122 +25,124 @@ all_device_ids = ','.join(device_name_to_id.keys()) # joins all device ids into 
 
 
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-# user database with username, password, and role
-users = {
-    'c2r': {'password': 'c2r', 'role': 'user'},
-    'admin': {'password': 'c2r', 'role': 'admin'}
-}
-
-
-
-class User(UserMixin): # flask's user model
-    def __init__ (self, username): # initializes the user class, its called when a new instance is created of User
-        self.id = username # sets users id to their username
-        self.role = users[username]['role'] # based on the users dictionary, it assigns their role 
-
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User(user_id)
-
-
-
-@app.route('/form_login', methods=['POST', 'GET']) # route can handle both types of request. might just leave it as ['POST']
-def login():
-    if request.method == 'POST': # checks if the method is a POST request, essentially determines if the user submits the form
-        username = request.form['username'] # gets the information user submits for their username credentials
-        password = request.form['password'] # gets the information user submits for their password credentials
-
-        # the following if else checks to see if the usrname and password the user submits is in the database, if not it will recieve an error message as shown in line 28
-        if username in users and users[username]['password'] == password:
-            user = User(username)
-            login_user(user)  
-            return redirect(url_for('index'))  
-        else:
-            return render_template('login.html', info='Invalid Credentials. Please contact IT.')
-    return render_template("index.html") # renders login page for smooth request 
-
-
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
-    if 'username' not in session:  # Check if the user is authenticated
-        return redirect(url_for('login'))
+    global peplink_token_error, search_submitted, device_data, api_error, start_date, end_date, ie_token_error
+    peplink_token_error = False
+    search_submitted = False
+    device_data = None
+    api_error = None
+    start_date = None
+    end_date = None
+    ie_token_error = False
+    
 
     if request.method == 'POST':
+        search_submitted = True
         start_date = request.form['start_date']
         end_date = request.form['end_date']
-        token, token_error = fetch_token_with_error_handling()
+        token = fetch_peplink_token()
+        accessToken = fetch_ie_token()
 
-        if token_error:
-            return render_template('index.html', token_error=True)
+        if token and accessToken is None:
+            peplink_token_error = True
+            ie_token_error = True
+        else:
+            response = fetch_device_data(start_date, end_date)
+            if isinstance(response, dict) and 'error' in response:
+                api_error = response['error']
+            else:
+                device_data = response
+    return render_template('index.html', devices=device_data, start_date=start_date, end_date=end_date, search_submitted=search_submitted, peplink_token_error=peplink_token_error, api_error=api_error, ie_token_error=ie_token_error)
 
-        device_data, api_error = fetch_device_data_with_error_handling(start_date, end_date, token)
-
-        return render_template('index.html', devices=device_data, start_date=start_date, end_date=end_date, search_submitted=True, api_error=api_error)
-
-    # GET request or initial page load
-    return render_template('index.html')
 
 
 
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
-    logout_user()
+    session.pop('username', None) 
     return redirect(url_for('login'))  # redirects to login page 
-
     
 
 
-def fetch_token(): # this function fetches the data from the env and constantly regenerates access tokens from InControl2
+def fetch_peplink_token(): # this function fetches the data from the env and constantly regenerates access tokens from InControl2
     client_id = os.getenv('client_id')
     client_secret = os.getenv('client_secret')
 
     data = {
-        'client_id': client_id,
-        'client_secret': client_secret,
-        'grant_type': 'client_credentials'
-    }
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'grant_type': 'client_credentials'
+            }
 
     url = 'https://api.ic.peplink.com/api/oauth2/token'
     response = requests.post(url, data=data)
 
     if response.status_code == 200:
-        return response.json().get('access_token')
+        peplinkAccessToken = response.json().get('access_token')
+        # Fetch the Peplink token
+        return peplinkAccessToken
     else:
         log_message = f"Failed to fetch token. Status Code: {response.status_code}, Response: {response.text}"
         with open('token_fetch_log.txt', 'a') as log_file:  # 'a' opens the file in append mode to preserve existing data (errors can conccur as well)
             log_file.write(f"{datetime.now()}: {log_message}\n")  # writes the current datetime and log message
 
         return None
+    
 
 
-def fetch_token_with_error_handling(): # wrapper function for error handling. it calls fetch_token(), then used in index which then determined how the page will render if True
-    token = fetch_token()
-    if token is None:
-        return None, True # if token is None then token_error is True
-    return token, False  # if token is returned then token_error is False
+def fetch_ie_token():
+    IDENT = os.getenv('IDENT')
+    IE_USERNAME =os.getenv('IE_USERNAME')
+    IE_PASSWORD = os.getenv('IE_PASSWORD')
+
+    url = 'https://webapi1.ielightning.net/api/v1/Authentication/GetAccessToken'
+    body = {
+            'ident': IDENT,
+            'username': IE_USERNAME,
+            'password': IE_PASSWORD
+            }
+    
+    headers = {'Content-Type': 'application/json'} 
+    response = requests.post(url=url, data=json.dumps(body), headers=headers)
+
+    
+    if response.status_code == 200:
+        ie_token = response.json().get('accessToken')
+        # Print the access token for debugging
+        #print("IE Access Token:", ie_token)
+        return ie_token
+        
+    else:
+        log_message = f"Failed to fetch IE token. Status code:{response.status_code}, Response: {response.text}"
+        with open ('token_fetch_log.txt', 'a') as log_file:
+            log_file.write(f"{datetime.now()}: {log_message}\n")
+        return None
 
 
 
-def fetch_device_data(start_date_str, end_date_str, token):
+
+def fetch_device_data(start_date_str, end_date_str):
+    # Fetch the Peplink token inside the function
+    peplinkAccessToken = fetch_peplink_token()
+    if not peplinkAccessToken:
+        # print("Failed to fetch Peplink token. Cannot proceed with API calls.")
+        return None
     # start and end_date these functions iterate over each day, then it checks if device ID is present in dictionary. if it is not -
     # - present it means its the first time this devices data is being processed for that date range. In that case, it will create a new entry in the dictionary.
     # if present, it processes data for each day and updates the data accordingly 
     start_date = datetime.strptime(start_date_str, '%Y-%m-%d') - timedelta(days=1) # subtracts day  
     end_date = datetime.strptime(end_date_str, '%Y-%m-%d') - timedelta(days=1) # subtracts day so that the end date can be set to the exact date the user inputs
+    # Assuming start_date_str and end_date_str are defined elsewhere in your script
     devices_data = {} 
 
     while start_date <= end_date:
         print(f"Processing date: {start_date}")
         formatted_date = start_date.strftime('%Y-%m-%d') + "T00:00:01"
         api_url = f'https://api.ic.peplink.com/rest/o/t00nnt/bandwidth_per_device?type=daily&report_date={formatted_date}&wan_id=2&device_ids={all_device_ids}&include_details=true&show_devices_with_usages_only=true'
+        print(f"Using Token Pepwave: '{peplinkAccessToken}'")
         headers = {
-                    'Authorization': f'Bearer {token}', 
+                    'Authorization': f'Bearer {peplinkAccessToken}', 
                     'Content-Type': 'application/json', 
                     'Accept': 'application/json',
                 }
@@ -173,21 +175,17 @@ def fetch_device_data(start_date_str, end_date_str, token):
     return prepare_display_data(devices_data)
 
 
-
-def fetch_device_data_with_error_handling(start_date, end_date, token): # wrapper function which serves the same way as fetch_token_with_error_handling() 
-    response = fetch_device_data(start_date, end_date, token)
-    if isinstance(response, dict) and 'error' in response:
-        return None, response['error']  # device data is None and api_error is the error message
-    return response, None  # device data is returned and api_error is None
-
-
-
 def fetch_location_name(device_name):
+        
+        ieAccessToken = fetch_ie_token() # get ie access token from fetch token function
+        if not ieAccessToken:
+            print("Failed to fetch IE token. Cannot proceed with API calls.")
+            return None
     # Check if the current user is an 'admin'
-    if 'username' in session and users[session['username']]['role'] == 'admin':
         api_url = f'https://webapi1.ielightning.net/api/v1/Inventory/StockItemsPage/StockItem/GetStockItemByBarCode?barCode={device_name}'
+        #print(f"Using token: '{ieAccessToken}'")
         headers = {
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJbnRlcm5hbENvbnRhY3RJZCI6IjMzNjQiLCJDb21wYW55SWQiOiI3NzQiLCJJZGVudCI6ImNob29zZTJyZW50IiwiQnVpbGRVbmlxdWVJZCI6IjQ1MDEiLCJSZWZyZXNoVG9rZW4iOiJCaWRvM3g0QnF5ek1pTEN1NENaaTBBPT0iLCJPZmZpY2VBY2Nlc3NJZHMiOiI0NTMsNDU0IiwibmJmIjoxNzA2NzkwMTY1LCJleHAiOjE3MDY4NzY1NjUsImlhdCI6MTcwNjc5MDE2NSwiaXNzIjoiaWVsaWdodG5pbmcubmV0In0.NA-uK0sHTQovuMMFxy8Whj_40oWGkmC5dsXQkG2MVTY',
+            'Authorization': f'Bearer {ieAccessToken}',
             'Content-Type': 'application/json',
             'Accept': 'application/json',
         }
@@ -196,20 +194,17 @@ def fetch_location_name(device_name):
             response.raise_for_status()
 
             data = response.json()
+            print(data)
             location_name = data.get('locationName', 'Unknown')
             job_number = location_name
             return location_name[0:12], job_number[6:12]
 
         except requests.HTTPError as http_err:
             print(f"HTTP error occurred: {http_err}")
-            return 'Unknown'
+            return 'Unknown', 'Unknown'
         except Exception as err:
             print(f"An error occurred: {err}")
-            return 'Unknown'
-    else:
-        print("Access denied: User does not have permission to access this function.")
-        return None, None  # or any appropriate response indicating access is denied
-
+            return 'Unknown', 'Unknown'
 
 def prepare_display_data(devices_data):
     # processes devices_data dictionary to prepare it for display
@@ -221,19 +216,12 @@ def prepare_display_data(devices_data):
         total_gb = round(data['total_up'] + data['total_down'], 2)
         if total_gb >= 3.0:
             # checks if user is an admin before calling fetch_location_name
-            if 'username' in session and users[session['username']]['role'] == 'admin':
-                location_name, job_number = fetch_location_name(data['name'])
-                if location_name is not None and job_number is not None:
-                    display_data.append((data['name'], total_gb, location_name, job_number))
-            else:
-                # non admin user roles can only see this information 
-                display_data.append((data['name'], total_gb))
+            location_name, job_number = fetch_location_name( data['name']) # makes argument to call location name function
+            print(job_number)
+            display_data.append((data['name'], total_gb, location_name, job_number))
 
     display_data.sort(key=lambda x: x[1], reverse=True) # sorts in ascending in order
     return display_data #returns sorted list 
-
-
-
 
 
 
